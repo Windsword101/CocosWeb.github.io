f50bd0818678f341b6db05d56d332cc4f0d4b212
Windsword101 <63327487+Windsword101@users.noreply.github.com>
2023-10-19 18:41:17 +0800

    更新002

---
+++ src/settings.d8e97.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/MainGame.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"d6080",main:"5852e"}};
