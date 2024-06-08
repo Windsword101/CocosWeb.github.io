b325fe6f96a77d6320d4ba4a8b8077d69c09d7d2
Windsword101 <63327487+Windsword101@users.noreply.github.com>
2024-06-08 08:47:08 +0800

    NEW

---
+++ src/settings.015a1.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/StartGame.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"f632d",main:"ddc79"}};
