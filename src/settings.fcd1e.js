ea2215e3d91dcee880f54e2cf95d33331eedb805
Windsword101 <63327487+Windsword101@users.noreply.github.com>
2024-06-13 01:18:39 +0800

    更新

--- src/settings.fcd1e.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/MJ/Scenes/StartGame.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d17e2",resources:"a0724",main:"a4519"}};
